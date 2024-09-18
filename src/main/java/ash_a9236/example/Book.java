111287863e964035e8dbb8aedc5096211441fd53
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-09-18 11:18:59 -0400

    getters, setters, toString

--- src/main/java/ash_a9236/example/Book.java
+++ src/main/java/ash_a9236/example/Book.java
@@ -60,19 +60,6 @@ public class Book {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return bookID == book.bookID && availableCopies == book.availableCopies && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(bookID, title, author, availableCopies);
-    }
-
-    @Override
     public String toString() {
         return "Book" + bookID +
                 "\nTitle : " + title + '\'' +
